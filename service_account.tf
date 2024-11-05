7e1f95c2b281aac006d74f726769a1ba99365f3d
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-11-05 15:03:18 +0900

    add role for artifact registry writer

--- service_account.tf
+++ service_account.tf
@@ -7,10 +7,19 @@ resource "google_service_account" "terraform_test_sa" {
 }
 
 # --------------------------------------------------
-# IAM role
+# IAM role for gcs_access
 # --------------------------------------------------
 resource "google_project_iam_member" "gcs_access" {
   project = var.project_id
   role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.terraform_test_sa.email}"
+}
+
+# --------------------------------------------------
+# IAM role for artifact_registry_writer
+# --------------------------------------------------
+resource "google_project_iam_member" "artifact_registry_writer" {
+  project = var.project_id
+  role    = "roles/artifactregistry.writer"
   member  = "serviceAccount:${google_service_account.terraform_test_sa.email}"
 }
