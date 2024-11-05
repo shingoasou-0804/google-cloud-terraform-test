2945d47b5b321f01aa38ba29e59eac77e9ebada5
shingoasou-0804 <122670383+shingoasou-0804@users.noreply.github.com>
2024-11-05 11:21:38 +0900

    add google cloud storage

---
+++ gcs.tf
@@ -0,0 +1,22 @@
+# --------------------------------------------------
+# Google Cloud Storage destination bucket
+# --------------------------------------------------
+resource "google_storage_bucket" "destination_bucket" {
+  name          = var.bucket_name
+  location      = var.bucket_location
+  storage_class = "STANDARD"
+  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30
+    }
+  }
+}
